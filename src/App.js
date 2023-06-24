f8cd32a968eb18928388d7b8180de65105a29c49
Laura McGowan <prime.lmcg@gmail.com>
2023-06-24 15:28:39 -0500

    updating state/view example

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ export default function App() {
 
   return (
     <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+
       {isOpen && (
         <div className="steps">
           <div className="numbers">
